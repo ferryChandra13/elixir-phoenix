8af71cd56ccf71fa74064fe760c7d55a08b2f001
ferryChandra13 <126833836+ferryChandra13@users.noreply.github.com>
2024-03-17 22:17:48 +0800

    add post router

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,14 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Alice", email: "LsS3M@example.com"},
+      %{id: 2, name: "Bob", email: "3wPpF@example.com"},
+      %{id: 3, name: "Eve", email: "5i8wv@example.com"},
+    ]
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
