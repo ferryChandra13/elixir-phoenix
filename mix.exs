d4706a1de758eccc1d86de645d431bc5841ee9f6
ferryChandra13 <126833836+ferryChandra13@users.noreply.github.com>
2024-03-22 17:40:19 +0800

    add dotenv package (#4)

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Forum.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:httpoison, "~> 2.2"}
+      {:httpoison, "~> 2.2"},
+      {:dotenv, "~> 3.1"}
     ]
   end
 
