f8a4feffb5920ded2a9108a8d2111d9d05fd7305
BenedictTan95 <127200083+BenedictTan95@users.noreply.github.com>
2024-03-22 15:23:07 +0800

    Add radar charts to /posts liveview (#3)
    * Add radar charts to /posts liveview
    
    * Update page_live.html.heex
    
    * fix sizing bug
    
    * Update app.js
    
    ---------
    
    Co-authored-by: ferryChandra13 <126833836+ferryChandra13@users.noreply.github.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,18 +18,93 @@
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
 import "phoenix_html"
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import { Socket } from "phoenix"
+import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Chart from 'chart.js/auto'; // import ChartJS library
+
+// Set up ChartJS hook for LiveView
+let hooks = {}
+hooks.ChartJS = {
+  chart: null,
+
+  // Define mounted callback
+  mounted() {
+    // Define chart type and variables to be sent from LiveView
+    const ctx = this.el.getContext('2d');
+    const userName = JSON.parse(this.el.getAttribute('data-user-name'))
+    const labels = JSON.parse(this.el.getAttribute('data-labels'));
+    const dataPoints = JSON.parse(this.el.getAttribute('data-scores'));
+    const data = {
+      type: 'radar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: userName,
+          data: dataPoints
+        }]
+      },
+      // Set options for chart
+      options: {
+        responsive: false,
+        maintainAspectRatio: true,
+        pointRadius: 0,
+        borderWidth: 0,
+        backgroundColor: 'rgba(255, 192, 203, 0.6)',
+        scales: {
+          r: {
+            ticks: {
+              display: false,
+            },
+            // Set skill label size
+            pointLabels: {
+              font: {
+                size: 18
+              }
+            },
+          },
+        },
+        plugins: {
+          // Set employee font size
+          legend: {
+            labels: {
+              font: {
+                size: 25
+              }
+            }
+          }
+        }
+      }
+    };
+    this.chart = new Chart(ctx, data);
+  },
+
+  // Define updated callback
+  updated() {
+    let canvas = this.el
+    let userName = JSON.parse(canvas.getAttribute("data-user-name"))
+    let labels = JSON.parse(canvas.getAttribute("data-labels"))
+    let dataPoints = JSON.parse(canvas.getAttribute("data-scores"))
+
+    if (this.chart) {
+      this.chart.data.datasets[0].label = userName;
+      this.chart.data.labels = labels;
+      this.chart.data.datasets[0].data = dataPoints;
+      this.chart.update();
+    }
+  }
+}
+
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {_csrf_token: csrfToken}
+  hooks: hooks, // Add our ChartJS hook
+  params: { _csrf_token: csrfToken }
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
@@ -41,4 +116,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
